e4af8b84ae3281c54d6e9979278dc1fd1ca447df
Meek Karan <meekkaran@gmail.com>
2021-07-19 17:55:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class  App extends Component{
+  render(){
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-6">column number one</div>
+          <div className="col-6">
+            <span>
+              <i className="fas fa-home"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
