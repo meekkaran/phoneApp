8214249408856069e546602a129802e927247f91
Meek Karan <meekkaran@gmail.com>
2021-07-26 12:45:08 +0300

    working on products details page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { Switch,Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
