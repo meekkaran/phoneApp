b6123b36b65b681b437fbd6e7c458df99b2e7a2e
Meek Karan <meekkaran@gmail.com>
2021-07-26 20:00:02 +0300

    working on products details

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
+import { PropTypes } from 'prop-types';
  
 export default class Product extends Component {
     render() {
@@ -10,8 +11,12 @@ export default class Product extends Component {
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3
             my-3">
                 <div className="card">
-                    <div className="img-container p-5" 
-                     onClick={ () => console.log('you clicked me on the image container')}>
+                    <ProductConsumer>
+                       {(value) =>(<div
+                        className="img-container p-5" 
+                        onClick={ () =>
+                            value.handleDetail(id)
+                         }>
                         <Link to ="/details">
                             <img src={img} alt="product" className="card-img-top" />
                         </Link>
@@ -19,7 +24,7 @@ export default class Product extends Component {
                            className="cart-btn" 
                            disabled={inCart ? true :false} 
                            onClick={()=>{
-                               console.log('added to cart');
+                               value.addToCart(id)
                              }}
                         >
                             {inCart?(
@@ -30,7 +35,9 @@ export default class Product extends Component {
                             <i className="fas fa-cart-plus"/>
                             )}
                         </button>   
-                    </div>
+                    </div>)}
+                    
+                    </ProductConsumer>
                     {/* card footer */}
                     <div className="card-footer d-flex 
                     justify-content-between">
@@ -44,6 +51,16 @@ export default class Product extends Component {
             </ProductWrapper>
         );
     }
+}
+
+Product.propTypes = {
+    product:PropTypes.shape({
+        id:PropTypes.number,
+        img:PropTypes.string,
+        title:PropTypes.string,
+        price:PropTypes.number,
+        inCart:PropTypes.bool
+    }).isRequired
 }
 
 const ProductWrapper = styled.div`
@@ -85,5 +102,14 @@ const ProductWrapper = styled.div`
     color:var(--mainWhite);
     font-size:1.4rem;
     border-radius:0.5rem 0 0 0;
+    transform:translate(100%,100%);
+    transition: all 1s linear;
+}
+.img-container:hover .cart-btn{
+    transform:translate(0, 0);
+}
+.cart-btn:hover{
+    color: var(--mainBlue);
+    cursor:pointer;
 }
 `;
--- src/context.js
+++ src/context.js
@@ -7,15 +7,39 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
 state={
-    products:storeProducts,
+    products:[],
     detailProduct:detailProduct
+};
+componentDidMount(){
+    this.setProducts();
 }
-handleDetail = () => {
-    console.log('hello from detail');
+setProducts = () =>{
+    let tempProducts = [];
+    storeProducts.forEach(item =>{
+        const singleItem = {...item};
+        tempProducts = [...tempProducts, singleItem]
+    })
+    this.setState(()=>{
+        return {products:tempProducts}
+    })
 }
-addToCart = () => {
-    console.log('hello from add to cart');
+
+getItem = id =>{
+    const product = this.state.products.find(item => item.id === id);
+    return product;
 }
+
+handleDetail = id => { 
+    const product = this.getItem(id);
+    this.setState(()=>{
+        return {detailProduct:product}
+    }) 
+};
+
+addToCart = (id) => {
+    console.log(`hello from add to cart.id is ${id}`);
+}
+
 
     render() {
         return (
